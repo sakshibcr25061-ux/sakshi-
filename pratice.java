a7efc9ae6fe7615ae6fa6a78cdef5a60a43ef342
sakshibcr25061-ux <sakshi.bcr25061@imsnoida.com>
2026-04-17 18:05:35 +0530

    Add files via upload

---
+++ pratice.java
@@ -0,0 +1,15 @@
+public class pratice {
+    public static void main(String[] args) {
+        String name = "Hello world";
+        String name2 = "java";
+        System.out.println(name);
+        System.out.println(name.toUpperCase());
+        System.out.println(name2.toLowerCase());
+        System.out.println(name. toLowerCase());
+        System.out.println(name.compareTo(name2 ));
+        System.out.println(name.equals(name2));
+        System.out.println(name.concat("!"));
+    
+
+    }
+}
